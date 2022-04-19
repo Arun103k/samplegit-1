c277be19f123e518cdde78b009dade27b2771331
“Arun20” <queenprince20@gmail.com>
2022-04-19 06:36:56 +0000

    2ndcommit

--- samplegit.py
+++ samplegit.py
@@ -12,8 +12,8 @@ print(Dict)
 
 # Adding set of values
 # to a single Key
-Dict['Value_set'] = 2, 3, 4
-print("\nDictionary after adding 3 elements: ")
+Dict['Value_set'] = 2, 3
+print("\nDictionary after adding 2 elements: ")
 print(Dict)
 
 # Updating existing Key's Value
@@ -21,8 +21,4 @@ Dict[2] = 'Welcome'
 print("\nUpdated key value: ")
 print(Dict)
 
-# Adding Nested Key value to Dictionary
-Dict[5] = {'Nested' :{'1' : 'Life', '2' : 'Geeks'}}
-print("\nAdding a Nested Key: ")
-print(Dict)
 
